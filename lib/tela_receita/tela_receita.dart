01095ffd69092093b45ac335e2a67735e53132c7
Gabrie-git <marcus.albernaz@estudante.iftm.edu.br>
2024-11-12 23:18:35 -0300

    Atualização do Gemini

--- lib/tela_receita/tela_receita.dart
+++ lib/tela_receita/tela_receita.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:flutter_gemini/flutter_gemini.dart';
 
 class ReceitaScreen extends StatefulWidget {
   const ReceitaScreen({super.key});
@@ -12,125 +15,159 @@ class _ReceitaScreenState extends State<ReceitaScreen>
     with AutomaticKeepAliveClientMixin {
   @override
   bool get wantKeepAlive => true;
+    final gemini = Gemini.instance;
+    String output = '';
 
-  final List<Map<String, String>> _receitas = [
-    {'titulo': 'Receita 1', 'descricao': 'Descrição da receita 1'},
-    {'titulo': 'Receita 2', 'descricao': 'Descrição da receita 2'},
-    {'titulo': 'Receita 3', 'descricao': 'Descrição da receita 3'},
-  ];
+  final String apiKey =
+      '';
+  final TextEditingController _controller = TextEditingController();
+  String _response = '';
+  bool _isLoading = false;
+StreamController<Candidates> _streamController = StreamController<Candidates>.broadcast();
+
+  @override
+  void initState() {
+    super.initState();
+  
+  }
+  
+
+  Future<void> obterReceita(String input) async {
+    if (input.isEmpty) return;
+
+   
+
+    setState(() {
+      _controller.text = '';
+      _isLoading = true;
+      _response = '';
+    });
+
+    final msg =
+        '$input';
+    
+
+
+
+    try {
+gemini.streamGenerateContent(msg,
+
+generationConfig: GenerationConfig(
+
+  
+)
+).listen((value) {
+
+  final ot = value.output??'';
+  
+  setState(() {
+    output += ot.toString();
+  });
+});
+
+//     final teste = await   gemini.text(msg,
+//     generationConfig: GenerationConfig(
+//       maxOutputTokens: 300
+//     )
+//     )
+//   ;
+
+// final teste2 = teste?.toJson();
+//   log(teste?.output??'aaa');
+
+//   output = teste?.output??'';
+  //     gemini.streamGenerateContent(msg)
+  // .listen((value) {
+  //   print(value.output);
+  // }).onError((e) {
+  //   log('streamGenerateContent exception', error: e);
+  // });
+
+
+      // final response = await openAI.onChatCompletion(request: request);
+      setState(() {
+        //  _response = response?.choices.last.message?.content.trim() ?? 'Erro ao processar resposta';
+        _isLoading = false;
+      });
+    } catch (error, stacktrace) {
+      debugPrint('Erro ao obter resposta: $error');
+      debugPrint('Stacktrace: $stacktrace');
+      setState(() {
+        _response = 'Erro de conexão ou de processamento. Tente novamente.';
+        _isLoading = false;
+      });
+    }
+  }
+  @override
+  void dispose() {
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     super.build(context);
     return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: const Size.fromHeight(60.0),
-        child: AppBar(
-          title: const Text(
-            'Receitas',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 30,
-              fontFamily: 'Lemonada',
-              fontWeight: FontWeight.w300,
-              height: 0,
-            ),
+      appBar: AppBar(
+        title: const Text(
+          'Receitas',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 30,
+            fontFamily: 'Lemonada',
+            fontWeight: FontWeight.w300,
           ),
-          backgroundColor: Colors.transparent,
-          elevation: 0,
-          flexibleSpace: Container(
-            decoration: ShapeDecoration(
-              color: const Color(0xFF1693A5),
-              shape: RoundedRectangleBorder(
-                side: BorderSide(
-                  width: 1,
-                  color: Colors.black.withOpacity(0.2),
-                ),
-              ),
-            ),
-          ),
-          actions: [
-  IconButton(
-    icon: FaIcon(FontAwesomeIcons.utensils), // Removido `const`
-    onPressed: () {
-      // Ação para notificações ou qualquer outra funcionalidade
-    },
-  ),
-],
-
         ),
+        backgroundColor: const Color(0xFF1693A5),
       ),
-      body: ListView.builder(
-        itemCount: _receitas.length,
-        itemBuilder: (context, index) {
-          final receita = _receitas[index];
-          return Card(
-            margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-            child: ListTile(
-              title: Text(
-                receita['titulo'] ?? '',
-                style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-              ),
-              subtitle: Text(receita['descricao'] ?? ''),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => DetalhesReceitaScreen(
-                      titulo: receita['titulo']!,
-                      descricao: receita['descricao']!,
+      body: Column(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: _response.isNotEmpty
+                  ? Text(
+                      _response,
+                      style: const TextStyle(fontSize: 16),
+                    )
+                  :  Center(
+                      child:
+                          AnimatedContainer(
+                            
+                            duration: Duration(seconds: 1),
+                            child: SingleChildScrollView(
+                              scrollDirection: Axis.vertical  ,
+                              child: Text(output.isEmpty? "Qual a boa para hoje?":output)),),),
+            ),
+          ),
+          if (_isLoading)
+            const Padding(
+              padding: EdgeInsets.all(16.0),
+              child: CircularProgressIndicator(),
+            ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _controller,
+                    decoration: const InputDecoration(
+                      labelText: 'Manda bala',
+                      border: OutlineInputBorder(),
                     ),
                   ),
-                );
-              },
+                ),
+                const SizedBox(width: 8),
+                IconButton(
+                  icon: const Icon(Icons.send),
+                  onPressed: () {
+                    obterReceita(_controller.text);
+                  },
+                ),
+              ],
             ),
-          );
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // Ação para adicionar nova receita
-        },
-        child: const Icon(Icons.add),
-        backgroundColor: const Color(0xFF1693A5),
-      ),
-    );
-  }
-}
-
-class DetalhesReceitaScreen extends StatelessWidget {
-  final String titulo;
-  final String descricao;
-
-  const DetalhesReceitaScreen({
-    Key? key,
-    required this.titulo,
-    required this.descricao,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(titulo),
-        backgroundColor: const Color(0xFF1693A5),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              titulo,
-              style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 16),
-            Text(
-              descricao,
-              style: const TextStyle(fontSize: 16),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
